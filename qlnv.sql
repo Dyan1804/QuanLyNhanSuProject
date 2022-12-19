6007b7f67e76a557c9890391fabae799830764ba
Dyan1804 <107311792+Dyan1804@users.noreply.github.com>
2022-12-19 10:01:56 +0700

    Add files via upload

---
+++ qlnv.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 19, 2022 at 04:00 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quanly`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qlnv`
+--
+
+CREATE TABLE `qlnv` (
+  `id` int(200) NOT NULL,
+  `fullname` varchar(250) NOT NULL,
+  `code` varchar(250) NOT NULL,
+  `class` varchar(250) NOT NULL,
+  `gmail` varchar(250) NOT NULL,
+  `birthday` date NOT NULL,
+  `sdt` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
